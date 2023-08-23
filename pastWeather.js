40465eafc94fef69778f097a6568d39db518e2e9
Saber Garibi <saber@sabergaribi.com>
2023-08-22 19:41:09 -0500

    date and temp is getting grabbed now to put on web

--- pastWeather.js
+++ pastWeather.js
@@ -1,5 +1,4 @@
 
-async function fetchData() {
 const baseUrl = "https://archive-api.open-meteo.com/v1/archive";
 //https://archive-api.open-meteo.com/v1/archive?latitude=52.5211&longitude=13.41111&start_date=2023-08-03&end_date=2023-08-17&hourly=temperature_2m&temperature_unit=fahrenheit&timezone=America%2FChicago
 //https://archive-api.open-meteo.com/v1/archive?latitude=52.52&longitude=13.41&start_date=2023-08-03&end_date=2023-08-17&hourly=temperature_2m&temperature_unit=fahrenheit&timezone=America%2FChicago
@@ -24,14 +23,33 @@ const end_date = "2020-06-30";
 //////////////////////https://archive-api.open-meteo.com/v1/archive?latitude=52.52&longitude=13.41&start_date=2023-08-04&end_date=2023-08-18&daily=temperature_2m_mean&temperature_unit=fahrenheit&windspeed_unit=mph&precipitation_unit=inch&timezone=America%2FChicago
 
 const pastWeatherUrl = `${baseUrl}?latitude=${selectedCity.split(',')[0]}&longitude=${selectedCity.split(',')[1]}&start_date=${start_date}&end_date=${end_date}&daily=temperature_2m_mean&temperature_unit=fahrenheit&windspeed_unit=mph&precipitation_unit=inch&timezone=America%2FChicago`;
+console.log(pastWeatherUrl);
 
-  const callAPI = await fetch(pastWeatherUrl);
-  const weatherData = await callAPI.json();
-  
-  const date = weatherData.daily.time[0];
-  console.log(date); 
+
+d3.json(pastWeatherUrl).then(function(earthquakeData) {
+  createFeatures(earthquakeData);
+});
 
 function createFeatures(earthquakeData) {
-  console.log(earthquakeData);
-}
-}
+  let time = earthquakeData.daily.temperature_2m_mean;
+  let frame = time.length; 
+  console.log(frame);
+  let counter = 0;
+  for(var i = 0; i < frame; i++){
+  let date = earthquakeData.daily.time[i];
+  let temp = earthquakeData.daily.temperature_2m_mean[i];
+  console.log(date + ":" +  temp);
+  counter++;
+
+  }
+  console.log(counter);
+
+  const weatherDataDiv = document.getElementById('weatherData');
+  weatherDataDiv.innerHTML = `
+    <p>Date: ${date} CST</p>
+    <p>Temp: ${temp} F CST</p>
+  `;
+
+  let metric = earthquakeData.daily_units.temperature_2m_mean;
+
+};
