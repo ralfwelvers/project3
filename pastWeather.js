5eaa947bb0b00cf3fd2655a84b0d26b94de9a22a
saberrg <86392267+saberrg@users.noreply.github.com>
2023-08-25 17:21:38 -0500

    proper line graph 2weeks past data

--- pastWeather.js
+++ pastWeather.js
@@ -1,10 +1,7 @@
 
-const baseUrl = "https://archive-api.open-meteo.com/v1/archive";
-
 const citySelect = document.getElementById('citySelect');
 const selectedCity = citySelect.value;
 let lat, lon; 
-let metric;
 
 if (selectedCity == "Chicago, IL"){
   lat = 41.875;
@@ -17,53 +14,38 @@ if (selectedCity == "Chicago, IL"){
   lon = -98.4936;
 }
 
-//show last two weeks from todays date for the selected city
-
-const startDateInput = document.getElementById('startDate');
-const endDateInput = document.getElementById('endDate');
-//const submitButton = document.getElementById('submit');
 const today = new Date();
 const start_date = new Date();
 
-start_date.setDate(start_date.getDate() - 300);
+//show last two weeks from todays date for the selected city
+start_date.setDate(start_date.getDate() - 14);
 today.setDate(today.getDate() - 1);
-const maxDate = today.toISOString().split('T')[0];
-startDateInput.max = maxDate;
-endDateInput.max = today.setDate(today.getDate() - 1);
-//https://weather.visualcrossing.com/VisualCrossingWebServices/rest/services/timeline/38.9697,-77.385?key=XVLF42ATVWX4DP9DKQT4DM9PW
-//https://weather.visualcrossing.com/VisualCrossingWebServices/rest/services/timeline/London,UK/2020-08-01/2020-08-25?key=XVLF42ATVWX4DP9DKQT4DM9PW
+
 let startDate = start_date.toISOString().split('T')[0];
   let endDate = today.toISOString().split('T')[0];
   console.log("Start Date:" + startDate);
   console.log("End Date: " + endDate);
-  const pastWeatherUrl2 = https://weather.visualcrossing.com/VisualCrossingWebServices/rest/services/timeline/London,UK/2020-08-01/2020-08-25?key=XVLF42ATVWX4DP9DKQT4DM9PW
-  const pastWeatherUrl = `${baseUrl}?latitude=${selectedCity.split(',')[0]}&longitude=${selectedCity.split(',')[1]}&start_date=${startDate}&end_date=${endDate}&daily=temperature_2m_mean&temperature_unit=fahrenheit&windspeed_unit=mph&precipitation_unit=inch&timezone=America%2FChicago`;
-  console.log(pastWeatherUrl);
-  d3.json(pastWeatherUrl).then(function(earthquakeData) {
-    metric = earthquakeData.daily_units.temperature_2m_mean;
+  let myCity = "Chicago,IL"
+  //put api key in config file
+  const pastWeatherUrl2 = `https://api.weatherbit.io/v2.0/history/daily?key=f5ef981c4d594b8b8be7501dc7da2fde&lat=29.4242&lon=-98.4936&start_date=${startDate}&end_date=${endDate}&units=I`;
+
+  console.log(pastWeatherUrl2);
+  d3.json(pastWeatherUrl2).then(function(earthquakeData) {
     createFeatures(earthquakeData);
   });
 
- 
-const weatherDataDiv = document.getElementById('weatherData');
-weatherDataDiv.innerHTML = "";
 function createFeatures(earthquakeData) {
-  let time = earthquakeData.daily.temperature_2m_mean;
-  let frame = time.length; 
+  console.log(earthquakeData);
+  let frame = earthquakeData.data.length; 
   console.log(frame);
-  const weatherDataDiv = document.getElementById('weatherData');
-  const weatherCardContainer = document.createElement('div');
-  weatherCardContainer.classList.add('weather-card-container');
-
 
 const chartData = {
   labels: [],
   series: [[]]
 };
 for (let i = 0; i < frame; i++){
-  let date = earthquakeData.daily.time[i];
-  let temp = earthquakeData.daily.temperature_2m_mean[i];
-  let formattedDate = '${date.getMonth()+1}/${date.getDate()}';
+  let date = earthquakeData.data[i].datetime;
+  let temp = earthquakeData.data[i].min_temp;
   chartData.labels.push(date);
   chartData.series[0].push(temp);
 }
@@ -84,22 +66,4 @@ const chartOptions = {
 }
 
 new Chartist.Line('#chartContainer', chartData, chartOptions);
-
-  // for(var i = 0; i < frame; i++){
-  // let date = earthquakeData.daily.time[i];
-  // let temp = earthquakeData.daily.temperature_2m_mean[i];
-  // const weatherEntry = `
-  // <div class="weather-card">
-  //   <div class="weather-card-header">
-  //     <p>Date: ${date} CST</p>
-  //   </div>
-  //   <div class="weather-card-content">
-  //     <p>Temperature: ${temp} F</p>
-  //   </div>
-  // </div>`;
-  // const weatherCard = document.createElement('div');
-  // weatherCard.innerHTML = weatherEntry;
-  // weatherCardContainer.appendChild(weatherCard);
-  // }
-  weatherDataDiv.appendChild(weatherCardContainer);
 };
